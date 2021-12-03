0df7cb956b9ecb148c902cd88e5773cec0735c3c
ManikantaGopisetti <manikantagopisetti2@gmail.com>
2021-12-03 11:12:35 +0530

    ADDED: JUnit Parameterised Test to validate multiple entry for the Email Address

---
+++ src/test/java/com/bridgelabz/UserRegistrationProgram/UserEmailValidationTest.java
@@ -0,0 +1,66 @@
+package com.bridgelabz.UserRegistrationProgram;
+
+import static org.junit.Assert.assertEquals;
+import java.util.Arrays;
+import java.util.Collection;
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.Parameterized;
+import org.junit.runners.Parameterized.Parameters;
+
+@RunWith(Parameterized.class)
+public class UserEmailValidationTest {
+	
+	String email;
+	boolean expected;
+	UserRegistration uRegistration;
+	
+	public UserEmailValidationTest(String email, boolean expected) {
+		super();
+		this.email = email;
+		this.expected = expected;
+	}
+	
+	@Parameters
+	public static Collection emailinput() {
+		return Arrays.asList(new Object[][] {
+			{"abc@yahoo.com",true},
+			{"abc-100@yahoo.com",true},
+			{"abc.100@yahoo.com",true},
+			{"abc111@abc.com",true},
+			{"abc-100@abc.net",true},
+			{"abc.100@abc.com.au",true},
+			{"abc@1.com",true},
+			{"abc@gmail.com.com",true},
+			{"abc+100@gmail.com",true},
+			{"abc",false},
+			{"abc@.com.my",false},
+			{"abc123@gmail.a",false},
+			{"abc123@.com",false},
+			{"abc123@.com.com",false},
+			{".abc@abc.com",false},
+			{"abc()*@gmail.com",false},
+			{"abc@%*.com",false},
+			{"abc..2002@gmail.com",false},
+			{"abc.@gmail.com",false},
+			{"abc@abc@gmail.com",false},
+			{"abc@gmail.com.1a",false},
+			{"abc@gmail.com.aa.au",false}});
+		
+		}
+	
+	@Before
+	public void userinit() {
+		uRegistration= new UserRegistration();
+	}
+	
+	@Test
+	@Parameters(name="input()")
+	public void emailValidation(){
+		
+		boolean output=uRegistration.emailValidation(email);
+		assertEquals(expected, output);
+	}
+
+}
--- src/test/java/com/bridgelabz/UserRegistrationProgram/UserValidationTest.java
+++ src/test/java/com/bridgelabz/UserRegistrationProgram/UserValidationTest.java
@@ -1,77 +1,74 @@
 package com.bridgelabz.UserRegistrationProgram;
 
 import static org.junit.Assert.assertEquals;
-
+import org.junit.Before;
 import org.junit.Test;
 
 public class UserValidationTest {
 	
+	UserRegistration uRegistration;	
+	
+	@Before
+	public void userinit() {
+		uRegistration= new UserRegistration();
+	}
+	
 	@Test
 	public void firstNameValidationHappy(){
-		UserRegistration uRegistration = new UserRegistration();
 		boolean output=uRegistration.firstNameValidation("Manikanta");
 		assertEquals(true, output);
 	}
 	
 	@Test
 	public void firstNameValidationsad(){
-		UserRegistration uRegistration = new UserRegistration();
 		boolean output=uRegistration.firstNameValidation("man");
 		assertEquals(false, output);
 	}
 	
 	@Test
 	public void lastNameValidationHappy(){
-		UserRegistration uRegistration = new UserRegistration();
 		boolean output=uRegistration.lastNameValidation("Man");
 		assertEquals(true, output);
 	}
 	
 	@Test
 	public void lastNameValidationsad(){
-		UserRegistration uRegistration = new UserRegistration();
 		boolean output=uRegistration.lastNameValidation("manikanta");
 		assertEquals(false, output);
 	}
 	
 	@Test
 	public void emailValidationHappy(){
-		UserRegistration uRegistration = new UserRegistration();
 		boolean output=uRegistration.emailValidation("manikanta@gmail.com");
 		assertEquals(true, output);
 	}
 	
 	@Test
 	public void emailValidationSad(){
-		UserRegistration uRegistration = new UserRegistration();
 		boolean output=uRegistration.emailValidation("mani@kanta@gmail.com");
 		assertEquals(false, output);
 	}
 	
 	@Test
 	public void mobileNumValidationHappy(){
-		UserRegistration uRegistration = new UserRegistration();
 		boolean output=uRegistration.mobileNumValidation("91 9959965321");
 		assertEquals(true, output);
 	}
 	
 	@Test
 	public void mobileNumValidationSad(){
-		UserRegistration uRegistration = new UserRegistration();
 		boolean output=uRegistration.mobileNumValidation("91 995996");
 		assertEquals(false, output);
 	}
 	
 	@Test
 	public void passwordValidationHappy(){
-		UserRegistration uRegistration = new UserRegistration();
 		boolean output=uRegistration.passwordValidation("Mani@1234");
 		assertEquals(true, output);
 	}
 	
 	@Test
 	public void passwordValidationSad(){
-		UserRegistration uRegistration = new UserRegistration();
 		boolean output=uRegistration.passwordValidation("mani1234");
 		assertEquals(false, output);
 	}
